bbcf25370188de422304f5c65f7b8ab5ec88cb93
AnyaSonina <89742951+AnyaSonina@users.noreply.github.com>
2023-04-28 18:59:22 +0100

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,35 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+var swiper = new Swiper(".mySwiper", {
+  breakpoints: {
+    
+     
+    600: {
+      slidesPerView: 1   
+      
+    },
+   
+    800: {
+      slidesPerView: 2      
+    },
+
+    900: {
+      slidesPerView: 3
+    }
+  },
+  
+  spaceBetween: 40,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  focusableElements: 'button',
+  watchSlidesProgress: true
 });
 
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-      document.body.classList.remove('nav-open');
-  })
-})
